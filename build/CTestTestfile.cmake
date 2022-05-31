f34f792f0d89da4974c96635fd4c95efa8a9a295
Andddy <229830916@qq.com>
2022-05-31 15:10:35 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros/Desktop/yjj/src
+# Build directory: /home/ros/Desktop/yjj/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
